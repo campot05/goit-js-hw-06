b11bcbbacc548659d46e9015526be9fcd365570b
campot05 <daniil.spectr@gmail.com>
2023-01-05 01:36:58 +0200

    task3 done

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,38 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// =============== option 1 ============================
+
+// const listItems = document.querySelector('.gallery');
+
+// const makeItem = ({ alt, url }) => {
+//   const item = document.createElement('li');
+//   const img = document.createElement('img');
+
+//   item.classList.add('gallery__item');
+
+//   img.classList.add('gallery__img');
+//   img.alt = alt;
+//   img.src = url;
+
+//   item.appendChild(img);
+
+//   return item;
+// };
+
+// const elements = images.map(makeItem);
+
+// listItems.append(...elements);
+
+// =============== option 2 ============================
+
+const listItems = document.querySelector('.gallery');
+
+const element = images
+  .map(({ url, alt }) => {
+    return `<li class='gallery__item'><img src=${url} class='gallery__img' alt='${alt}' width='330'></li>`;
+  })
+  .join('');
+
+listItems.insertAdjacentHTML('afterbegin', element);
