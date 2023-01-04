2d9c9800678a13aa9f484fd8f96a50965c3fd852
campot05 <daniil.spectr@gmail.com>
2023-01-05 00:13:53 +0200

    task1 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,7 @@
-const numberOfCategories = document.querySelectorAll('#categories .item');
-console.log(`Number of categories: ${numberOfCategories.length}`);
+const listItemsRef = document.querySelectorAll('.item');
+console.log(`Number of categories: ${listItemsRef.length}`);
+
+listItemsRef.forEach((item) => {
+  console.log(`Category: ${item.querySelector('h2').textContent}`);
+  console.log(`Element: ${item.querySelectorAll('li').length}`);
+});
