258234d1cdd62433c6fbe3c63cdacbaac4c9592d
campot05 <daniil.spectr@gmail.com>
2023-01-05 19:54:50 +0200

    task8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', formData);
+
+function formData(event) {
+  event.preventDefault();
+
+  const mail = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (mail === '' || password === '') {
+    return alert('Заполните все поля, пожалуйста');
+  }
+
+  const formData = {
+    mail,
+    password,
+  };
+
+  console.log(formData);
+
+  formEl.reset();
+}
