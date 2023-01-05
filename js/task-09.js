615840a401a075e53a089fd87ef4f1a30f3b278d
campot05 <daniil.spectr@gmail.com>
2023-01-05 20:07:58 +0200

    task9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const btnEl = document.querySelector('.change-color');
+const spanColorEl = document.querySelector('.color');
+const body = document.body;
+
+btnEl.addEventListener('click', () => {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  spanColorEl.textContent = getRandomHexColor();
+});
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
