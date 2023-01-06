1d0864640eda94ad147932e542c4de7886e6bc28
campot05 <daniil.spectr@gmail.com>
2023-01-06 12:46:47 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -6,12 +6,13 @@ const inputEl = document.querySelector('[type=number]');
 boxesEl.style.display = 'flex';
 boxesEl.style.flexWrap = 'wrap';
 
-createBtnEl.addEventListener('click', () => {
-  const items = createBoxes(inputEl.value);
-  boxesEl.append(...items);
-});
+createBtnEl.addEventListener('click', handler);
 
 destroyBtnEl.addEventListener('click', clearItems);
+
+function handler() {
+  createBoxes(inputEl.value);
+}
 
 function createBoxes(amount) {
   const collections = [];
@@ -25,7 +26,7 @@ function createBoxes(amount) {
     size += 10;
     collections.push(item);
   }
-  return collections;
+  boxesEl.append(...collections);
 }
 
 function clearItems() {
@@ -38,6 +39,49 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// ===== вроде норм =====
+
+// const boxesEl = document.querySelector('#boxes');
+// const createBtnEl = document.querySelector('[data-create]');
+// const destroyBtnEl = document.querySelector('[data-destroy]');
+// const inputEl = document.querySelector('[type=number]');
+
+// boxesEl.style.display = 'flex';
+// boxesEl.style.flexWrap = 'wrap';
+
+// createBtnEl.addEventListener('click', () => {
+//   const items = createBoxes(inputEl.value);
+//   boxesEl.append(...items);
+// });
+
+// destroyBtnEl.addEventListener('click', clearItems);
+
+// function createBoxes(amount) {
+//   const collections = [];
+//   let size = 30;
+//   for (let i = 0; i < amount; i += 1) {
+//     const item = document.createElement('div');
+//     item.style.height = `${size}px`;
+//     item.style.width = `${size}px`;
+//     item.style.marginRight = `10px`;
+//     item.style.backgroundColor = `${getRandomHexColor()}`;
+//     size += 10;
+//     collections.push(item);
+//   }
+//   return collections;
+// }
+
+// function clearItems() {
+//   boxesEl.innerHTML = '';
+//   inputEl.value = '';
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
 
 // ===== стрёмный вариант, бо элементы добавляются по одному, а это вроде не хорошо =======
 
